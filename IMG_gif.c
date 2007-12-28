585dbc926ccbaf56f387415d0c1c29fa3fb61f2b
Sam Lantinga <slouken@libsdl.org>
2007-12-28 16:43:56 +0000

    Updated version to 1.2.7  * Fixed buffer overflow in GIF loading code, discovered by Michael Skladnikiewicz

--- IMG_gif.c
+++ IMG_gif.c
@@ -418,6 +418,10 @@ LWZReadByte(SDL_RWops *src, int flag, int input_code_size)
     static int stack[(1 << (MAX_LWZ_BITS)) * 2], *sp;
     register int i;
 
+    /* Fixed buffer overflow found by Michael Skladnikiewicz */
+    if (input_code_size > MAX_LWZ_BITS)
+        return -1;
+
     if (flag) {
 	set_code_size = input_code_size;
 	code_size = set_code_size + 1;
