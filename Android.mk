3775b8c798483f74562345d62f06655abbd59a5e
Sam Lantinga <slouken@libsdl.org>
2011-01-10 10:55:09 -0800

    Added Android Makefile to create an SDL_image shared library

---
+++ Android.mk
@@ -0,0 +1,18 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := SDL_image
+
+LOCAL_CFLAGS := -I$(LOCAL_PATH)/../jpeg -I$(LOCAL_PATH)/../png -I$(LOCAL_PATH)/../sdl/include \
+				-DLOAD_PNG -DLOAD_JPG -DLOAD_GIF -DLOAD_BMP
+
+LOCAL_SRC_FILES := $(notdir $(wildcard $(LOCAL_PATH)/*.c))
+
+LOCAL_SHARED_LIBRARIES := SDL
+
+LOCAL_STATIC_LIBRARIES := png jpeg
+
+LOCAL_LDLIBS := -lz
+
+include $(BUILD_SHARED_LIBRARY)
