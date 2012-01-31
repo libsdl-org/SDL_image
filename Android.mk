853e80d4daa3c2effb786a2a5bbf173a08398c69
Sam Lantinga <slouken@libsdl.org>
2012-01-30 20:09:56 -0500

    Updated Android Makefile for SDL2

--- Android.mk
+++ Android.mk
@@ -2,7 +2,7 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 
-LOCAL_MODULE := SDL_image
+LOCAL_MODULE := SDL2_image
 
 LOCAL_CFLAGS := -I$(LOCAL_PATH)/../jpeg -I$(LOCAL_PATH)/../png -I$(LOCAL_PATH)/../SDL/include \
 	-DLOAD_JPG -DLOAD_PNG -DLOAD_BMP -DLOAD_GIF -DLOAD_LBM \
@@ -11,7 +11,7 @@ LOCAL_CFLAGS := -I$(LOCAL_PATH)/../jpeg -I$(LOCAL_PATH)/../png -I$(LOCAL_PATH)/.
 
 LOCAL_SRC_FILES := $(notdir $(filter-out %/showimage.c, $(wildcard $(LOCAL_PATH)/*.c)))
 
-LOCAL_SHARED_LIBRARIES := SDL
+LOCAL_SHARED_LIBRARIES := SDL2
 
 LOCAL_STATIC_LIBRARIES := png jpeg
 
