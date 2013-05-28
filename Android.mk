426e12a8357968a788600cbf323fb03678ecb07c
Sam Lantinga <slouken@libsdl.org>
2013-05-27 18:32:57 -0700

    Updated support for Android

--- Android.mk
+++ Android.mk
@@ -4,17 +4,112 @@ include $(CLEAR_VARS)
 
 LOCAL_MODULE := SDL2_image
 
-LOCAL_CFLAGS := -I$(LOCAL_PATH)/../jpeg -I$(LOCAL_PATH)/../png -I$(LOCAL_PATH)/../SDL/include \
-	-DLOAD_JPG -DLOAD_PNG -DLOAD_BMP -DLOAD_GIF -DLOAD_LBM \
-	-DLOAD_PCX -DLOAD_PNM -DLOAD_TGA -DLOAD_XCF -DLOAD_XPM \
-	-DLOAD_XV
+# Enable this if you want to support loading JPEG images
+# The library path should be a relative path to this directory.
+SUPPORT_JPG := true
+JPG_LIBRARY_PATH := external/jpeg-9
+
+# Enable this if you want to support loading PNG images
+# The library path should be a relative path to this directory.
+SUPPORT_PNG := true
+PNG_LIBRARY_PATH := external/libpng-1.6.2
+
+# Enable this if you want to support loading WebP images
+# The library path should be a relative path to this directory.
+SUPPORT_WEBP := true
+WEBP_LIBRARY_PATH := external/libwebp-0.3.0
+
+LOCAL_CFLAGS := -I$(LOCAL_PATH)/../SDL/include \
+	-DLOAD_BMP -DLOAD_GIF -DLOAD_LBM -DLOAD_PCX -DLOAD_PNM \
+    -DLOAD_TGA -DLOAD_XCF -DLOAD_XPM -DLOAD_XV
+LOCAL_CFLAGS += -O3 -fstrict-aliasing -fprefetch-loop-arrays
 
 LOCAL_SRC_FILES := $(notdir $(filter-out %/showimage.c, $(wildcard $(LOCAL_PATH)/*.c)))
 
+LOCAL_LDLIBS :=
+LOCAL_STATIC_LIBRARIES :=
 LOCAL_SHARED_LIBRARIES := SDL2
 
-LOCAL_STATIC_LIBRARIES := png jpeg
+ifeq ($(SUPPORT_JPG),true)
+    LOCAL_CFLAGS += -DLOAD_JPG -I$(LOCAL_PATH)/$(JPG_LIBRARY_PATH)
+    # We can include the sources directly so the user doesn't have to...
+    #LOCAL_STATIC_LIBRARIES += jpeg
+    LOCAL_CFLAGS += -DAVOID_TABLES
+    LOCAL_SRC_FILES += \
+        $(JPG_LIBRARY_PATH)/jaricom.c \
+        $(JPG_LIBRARY_PATH)/jcapimin.c \
+        $(JPG_LIBRARY_PATH)/jcapistd.c \
+        $(JPG_LIBRARY_PATH)/jcarith.c \
+        $(JPG_LIBRARY_PATH)/jccoefct.c \
+        $(JPG_LIBRARY_PATH)/jccolor.c \
+        $(JPG_LIBRARY_PATH)/jcdctmgr.c \
+        $(JPG_LIBRARY_PATH)/jchuff.c \
+        $(JPG_LIBRARY_PATH)/jcinit.c \
+        $(JPG_LIBRARY_PATH)/jcmainct.c \
+        $(JPG_LIBRARY_PATH)/jcmarker.c \
+        $(JPG_LIBRARY_PATH)/jcmaster.c \
+        $(JPG_LIBRARY_PATH)/jcomapi.c \
+        $(JPG_LIBRARY_PATH)/jcparam.c \
+        $(JPG_LIBRARY_PATH)/jcprepct.c \
+        $(JPG_LIBRARY_PATH)/jcsample.c \
+        $(JPG_LIBRARY_PATH)/jctrans.c \
+        $(JPG_LIBRARY_PATH)/jdapimin.c \
+        $(JPG_LIBRARY_PATH)/jdapistd.c \
+        $(JPG_LIBRARY_PATH)/jdarith.c \
+        $(JPG_LIBRARY_PATH)/jdatadst.c \
+        $(JPG_LIBRARY_PATH)/jdatasrc.c \
+        $(JPG_LIBRARY_PATH)/jdcoefct.c \
+        $(JPG_LIBRARY_PATH)/jdcolor.c \
+        $(JPG_LIBRARY_PATH)/jddctmgr.c \
+        $(JPG_LIBRARY_PATH)/jdhuff.c \
+        $(JPG_LIBRARY_PATH)/jdinput.c \
+        $(JPG_LIBRARY_PATH)/jdmainct.c \
+        $(JPG_LIBRARY_PATH)/jdmarker.c \
+        $(JPG_LIBRARY_PATH)/jdmaster.c \
+        $(JPG_LIBRARY_PATH)/jdmerge.c \
+        $(JPG_LIBRARY_PATH)/jdpostct.c \
+        $(JPG_LIBRARY_PATH)/jdsample.c \
+        $(JPG_LIBRARY_PATH)/jdtrans.c \
+        $(JPG_LIBRARY_PATH)/jerror.c \
+        $(JPG_LIBRARY_PATH)/jfdctflt.c \
+        $(JPG_LIBRARY_PATH)/jfdctfst.c \
+        $(JPG_LIBRARY_PATH)/jfdctint.c \
+        $(JPG_LIBRARY_PATH)/jidctflt.c \
+        $(JPG_LIBRARY_PATH)/jidctfst.S \
+        $(JPG_LIBRARY_PATH)/jidctint.c \
+        $(JPG_LIBRARY_PATH)/jquant1.c \
+        $(JPG_LIBRARY_PATH)/jquant2.c \
+        $(JPG_LIBRARY_PATH)/jutils.c \
+        $(JPG_LIBRARY_PATH)/jmemmgr.c \
+        $(JPG_LIBRARY_PATH)/jmem-android.c
+endif
 
-LOCAL_LDLIBS := -lz
+ifeq ($(SUPPORT_PNG),true)
+    LOCAL_CFLAGS += -DLOAD_PNG -I$(LOCAL_PATH)/$(PNG_LIBRARY_PATH)
+    # We can include the sources directly so the user doesn't have to...
+    #LOCAL_STATIC_LIBRARIES += png
+    LOCAL_SRC_FILES += \
+        $(PNG_LIBRARY_PATH)/png.c \
+        $(PNG_LIBRARY_PATH)/pngerror.c \
+        $(PNG_LIBRARY_PATH)/pngget.c \
+        $(PNG_LIBRARY_PATH)/pngmem.c \
+        $(PNG_LIBRARY_PATH)/pngpread.c \
+        $(PNG_LIBRARY_PATH)/pngread.c \
+        $(PNG_LIBRARY_PATH)/pngrio.c \
+        $(PNG_LIBRARY_PATH)/pngrtran.c \
+        $(PNG_LIBRARY_PATH)/pngrutil.c \
+        $(PNG_LIBRARY_PATH)/pngset.c \
+        $(PNG_LIBRARY_PATH)/pngtrans.c \
+        $(PNG_LIBRARY_PATH)/pngwio.c \
+        $(PNG_LIBRARY_PATH)/pngwrite.c \
+        $(PNG_LIBRARY_PATH)/pngwtran.c \
+        $(PNG_LIBRARY_PATH)/pngwutil.c
+    LOCAL_LDLIBS += -lz
+endif
+
+ifeq ($(SUPPORT_WEBP),true)
+    LOCAL_CFLAGS += -DLOAD_WEBP -I$(LOCAL_PATH)/$(WEBP_LIBRARY_PATH)/src
+    LOCAL_STATIC_LIBRARIES += webp
+endif
 
 include $(BUILD_SHARED_LIBRARY)
--- IMG_png.c
+++ IMG_png.c
@@ -78,15 +78,15 @@
 static struct {
     int loaded;
     void *handle;
-    png_infop (*png_create_info_struct) (png_structp png_ptr);
+    png_infop (*png_create_info_struct) (png_const_structrp png_ptr);
     png_structp (*png_create_read_struct) (png_const_charp user_png_ver, png_voidp error_ptr, png_error_ptr error_fn, png_error_ptr warn_fn);
     void (*png_destroy_read_struct) (png_structpp png_ptr_ptr, png_infopp info_ptr_ptr, png_infopp end_info_ptr_ptr);
-    png_uint_32 (*png_get_IHDR) (png_structp png_ptr, png_infop info_ptr, png_uint_32 *width, png_uint_32 *height, int *bit_depth, int *color_type, int *interlace_method, int *compression_method, int *filter_method);
-    png_voidp (*png_get_io_ptr) (png_structp png_ptr);
-    png_byte (*png_get_channels) (png_structp png_ptr, png_infop info_ptr);
-    png_uint_32 (*png_get_PLTE) (png_structp png_ptr, png_infop info_ptr, png_colorp *palette, int *num_palette);
-    png_uint_32 (*png_get_tRNS) (png_structp png_ptr, png_infop info_ptr, png_bytep *trans, int *num_trans, png_color_16p *trans_values);
-    png_uint_32 (*png_get_valid) (png_structp png_ptr, png_infop info_ptr, png_uint_32 flag);
+    png_uint_32 (*png_get_IHDR) (png_const_structrp png_ptr, png_const_inforp info_ptr, png_uint_32 *width, png_uint_32 *height, int *bit_depth, int *color_type, int *interlace_method, int *compression_method, int *filter_method);
+    png_voidp (*png_get_io_ptr) (png_const_structrp png_ptr);
+    png_byte (*png_get_channels) (png_const_structrp png_ptr, png_const_inforp info_ptr);
+    png_uint_32 (*png_get_PLTE) (png_const_structrp png_ptr, png_infop info_ptr, png_colorp *palette, int *num_palette);
+    png_uint_32 (*png_get_tRNS) (png_const_structrp png_ptr, png_infop info_ptr, png_bytep *trans, int *num_trans, png_color_16p *trans_values);
+    png_uint_32 (*png_get_valid) (png_const_structrp png_ptr, png_const_inforp info_ptr, png_uint_32 flag);
     void (*png_read_image) (png_structp png_ptr, png_bytepp image);
     void (*png_read_info) (png_structp png_ptr, png_infop info_ptr);
     void (*png_read_update_info) (png_structp png_ptr, png_infop info_ptr);
@@ -95,7 +95,7 @@ static struct {
     void (*png_set_packing) (png_structp png_ptr);
     void (*png_set_read_fn) (png_structp png_ptr, png_voidp io_ptr, png_rw_ptr read_data_fn);
     void (*png_set_strip_16) (png_structp png_ptr);
-    int (*png_sig_cmp) (png_bytep sig, png_size_t start, png_size_t num_to_check);
+    int (*png_sig_cmp) (png_const_bytep sig, png_size_t start, png_size_t num_to_check);
 #ifndef LIBPNG_VERSION_12
     jmp_buf* (*png_set_longjmp_fn) (png_structp, png_longjmp_ptr, size_t);
 #endif
@@ -110,7 +110,7 @@ int IMG_InitPNG()
             return -1;
         }
         lib.png_create_info_struct =
-            (png_infop (*) (png_structp))
+            (png_infop (*) (png_const_structrp))
             SDL_LoadFunction(lib.handle, "png_create_info_struct");
         if ( lib.png_create_info_struct == NULL ) {
             SDL_UnloadObject(lib.handle);
@@ -131,42 +131,42 @@ int IMG_InitPNG()
             return -1;
         }
         lib.png_get_IHDR =
-            (png_uint_32 (*) (png_structp, png_infop, png_uint_32 *, png_uint_32 *, int *, int *, int *, int *, int *))
+            (png_uint_32 (*) (png_const_structrp, png_const_inforp, png_uint_32 *, png_uint_32 *, int *, int *, int *, int *, int *))
             SDL_LoadFunction(lib.handle, "png_get_IHDR");
         if ( lib.png_get_IHDR == NULL ) {
             SDL_UnloadObject(lib.handle);
             return -1;
         }
         lib.png_get_channels =
-            (png_byte (*) (png_structp, png_infop))
+            (png_byte (*) (png_const_structrp, png_const_inforp))
             SDL_LoadFunction(lib.handle, "png_get_channels");
         if ( lib.png_get_channels == NULL ) {
             SDL_UnloadObject(lib.handle);
             return -1;
         }
         lib.png_get_io_ptr =
-            (png_voidp (*) (png_structp))
+            (png_voidp (*) (png_const_structrp))
             SDL_LoadFunction(lib.handle, "png_get_io_ptr");
         if ( lib.png_get_io_ptr == NULL ) {
             SDL_UnloadObject(lib.handle);
             return -1;
         }
         lib.png_get_PLTE =
-            (png_uint_32 (*) (png_structp, png_infop, png_colorp *, int *))
+            (png_uint_32 (*) (png_const_structrp, png_infop, png_colorp *, int *))
             SDL_LoadFunction(lib.handle, "png_get_PLTE");
         if ( lib.png_get_PLTE == NULL ) {
             SDL_UnloadObject(lib.handle);
             return -1;
         }
         lib.png_get_tRNS =
-            (png_uint_32 (*) (png_structp, png_infop, png_bytep *, int *, png_color_16p *))
+            (png_uint_32 (*) (png_const_structrp, png_infop, png_bytep *, int *, png_color_16p *))
             SDL_LoadFunction(lib.handle, "png_get_tRNS");
         if ( lib.png_get_tRNS == NULL ) {
             SDL_UnloadObject(lib.handle);
             return -1;
         }
         lib.png_get_valid =
-            (png_uint_32 (*) (png_structp, png_infop, png_uint_32))
+            (png_uint_32 (*) (png_const_structrp, png_const_inforp, png_uint_32))
             SDL_LoadFunction(lib.handle, "png_get_valid");
         if ( lib.png_get_valid == NULL ) {
             SDL_UnloadObject(lib.handle);
@@ -229,7 +229,7 @@ int IMG_InitPNG()
             return -1;
         }
         lib.png_sig_cmp =
-            (int (*) (png_bytep, png_size_t, png_size_t))
+            (int (*) (png_const_bytep, png_size_t, png_size_t))
             SDL_LoadFunction(lib.handle, "png_sig_cmp");
         if ( lib.png_sig_cmp == NULL ) {
             SDL_UnloadObject(lib.handle);
