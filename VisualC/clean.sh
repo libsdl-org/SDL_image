3dca7746de4a291373f8f3681a47735cde6cfedb
Sam Lantinga <slouken@libsdl.org>
2015-06-14 12:21:55 -0700

    Updated Visual Studio projects There is now a single solution used by Visual Studio 2010 and newer

--- VisualC/clean.sh
+++ VisualC/clean.sh
@@ -1,6 +1,3 @@
-find . -depth -type d -name 'Debug' -exec rm -rv {} \;
-find . -depth -type d -name 'Release' -exec rm -rv {} \;
-find . -depth -type d -name 'x64' -empty -exec rmdir {} \;
-find . -type f -name '*.user' -exec rm -v {} \;
-find . -type f -name '*.ncb' -exec rm -v {} \;
-find . -type f -name '*.suo' -exec rm -v {} \;
+#!/bin/sh
+find . -type f \( -name '*.user' -o -name '*.sdf' -o -name '*.ncb' -o -name '*.suo' \) -print -delete
+rm -rvf Win32 */Win32 x64 */x64
