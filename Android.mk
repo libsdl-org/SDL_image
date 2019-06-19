943c5c65a53046019603e3be0bea6d49d6795a70
Sam Lantinga <slouken@libsdl.org>
2019-06-19 07:36:02 -0700

    Updated to libweb-1.0.2

--- Android.mk
+++ Android.mk
@@ -9,12 +9,12 @@ JPG_LIBRARY_PATH := external/jpeg-9b
 # Enable this if you want to support loading PNG images
 # The library path should be a relative path to this directory.
 SUPPORT_PNG ?= true
-PNG_LIBRARY_PATH := external/libpng-1.6.35
+PNG_LIBRARY_PATH := external/libpng-1.6.37
 
 # Enable this if you want to support loading WebP images
 # The library path should be a relative path to this directory.
 SUPPORT_WEBP ?= true
-WEBP_LIBRARY_PATH := external/libwebp-0.6.0
+WEBP_LIBRARY_PATH := external/libwebp-1.0.2
 
 
 # Build the library
