e074552ebd810d6078a7959df60915760865565e
Ozkan Sezer <sezeroz@gmail.com>
2020-12-23 14:41:01 +0300

    external libs: updated libtiff to latest 4.2.0 release.

--- external/tiff-4.1.0/test/tiff2ps-PS2.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-#
-# Basic sanity check for tiffps with PostScript Level 2 output
-#
-PSFILE=o-tiff2ps-PS2.ps
-. ${srcdir:-.}/common.sh
-f_test_stdout "${TIFF2PS} -a -p -2" "${IMG_MINISWHITE_1C_1B}" "${PSFILE}"
-diff -I '%%\(CreationDate\|Title\):*' -u "${REFS}/${PSFILE}" "${PSFILE}" || exit 1
