8dfc9a5f8e431e469ea0c97819a41a9927f0a1b9
Sam Lantinga <slouken@libsdl.org>
2022-05-09 15:47:56 -0700

    Fixed cycling through images using showimage.c

--- showimage.c
+++ showimage.c
@@ -133,31 +133,32 @@ int main(int argc, char *argv[])
         SDL_SetWindowSize(window, w, h);
         SDL_ShowWindow(window);
 
-        while ( ! done ) {
+        done = 0;
+        while ( !done ) {
             while ( SDL_PollEvent(&event) ) {
                 switch (event.type) {
                     case SDL_KEYUP:
                         switch (event.key.keysym.sym) {
-                            case SDLK_LEFT:
-                                if ( i > 1 ) {
-                                    i -= 2;
-                                    done = 1;
-                                }
-                                break;
-                            case SDLK_RIGHT:
-                                if ( argv[i+1] ) {
-                                    done = 1;
-                                }
-                                break;
-                            case SDLK_ESCAPE:
-                            case SDLK_q:
-                                argv[i+1] = NULL;
-                            /* Drop through to done */
-                            case SDLK_SPACE:
-                            case SDLK_TAB:
+                        case SDLK_LEFT:
+                            if ( i > 1 ) {
+                                i -= 2;
+                                done = 1;
+                            }
+                            break;
+                        case SDLK_RIGHT:
+                            if ( argv[i+1] ) {
+                                done = 1;
+                            }
+                            break;
+                        case SDLK_ESCAPE:
+                        case SDLK_q:
+                            argv[i+1] = NULL;
+                        /* Drop through to done */
+                        case SDLK_SPACE:
+                        case SDLK_TAB:
                             done = 1;
                             break;
-                            default:
+                        default:
                             break;
                         }
                         break;
