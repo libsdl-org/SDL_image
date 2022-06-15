3438bb17c0e17d5bed7df7f03dc3ffefc886db1e
Ozkan Sezer <sezeroz@gmail.com>
2022-06-15 17:00:02 +0300

    minor tidy-up to configury.

--- autogen.sh
+++ autogen.sh
@@ -2,9 +2,8 @@
 
 set -e
 
-aclocal -I acinclude
-automake --foreign --include-deps --add-missing --copy
-autoconf
+"${ACLOCAL:-aclocal}" -I acinclude
+"${AUTOMAKE:-automake}" --foreign --include-deps --add-missing --copy
+"${AUTOCONF:-autoconf}"
 
-#./configure $*
 echo "Now you are ready to run ./configure"
