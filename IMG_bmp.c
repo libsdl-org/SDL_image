9a5bd2d522c8e0f5a92ba7c8c1bac123228611d0
Tad Hardesty <tad@platymuus.com>
2023-01-29 15:04:23 -0800

    Fix loading of 4-bit .ico files
    (cherry picked from commit b740b68946de9f3952617d252192fac90c6edb9e)

--- IMG_bmp.c
+++ IMG_bmp.c
@@ -685,7 +685,6 @@ LoadICOCUR_RW(SDL_RWops * src, int type, int freesrc)
         case 1:
         case 4:
             ExpandBMP = biBitCount;
-            biBitCount = 8;
             break;
         case 8:
             ExpandBMP = 8;
