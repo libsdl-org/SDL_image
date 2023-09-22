6db0ca691f7ea9f67a9a695e98b59980fbfe5523
Anonymous Maarten <anonymous.maarten@gmail.com>
2023-09-22 03:22:09 +0200

    cmake: don't install static vendored libraries when SDL3_image is built as a shared library
    The library is already built-in. No need for an extra static library.

--- cmake/test/main.c
+++ cmake/test/main.c
@@ -2,15 +2,31 @@
 #include <SDL3/SDL_main.h>
 #include <SDL3_image/SDL_image.h>
 
+#define TEST_INIT_FLAG(FLAG) do {                   \
+        if ((IMG_Init(FLAG) & FLAG) == FLAG) {      \
+            SDL_Log("IMG_Init("#FLAG") succeeded"); \
+        } else {                                    \
+            SDL_Log("IMG_Init("#FLAG") failed");    \
+        }                                           \
+    } while (0);
+
+#define FOREACH_INIT_FLAGS(X) \
+    X(IMG_INIT_JPG)           \
+    X(IMG_INIT_PNG)           \
+    X(IMG_INIT_TIF)           \
+    X(IMG_INIT_WEBP)          \
+    X(IMG_INIT_JXL)           \
+    X(IMG_INIT_AVIF)          \
+
 int main(int argc, char *argv[])
 {
     if (SDL_Init(0) < 0) {
-        SDL_Log("Could not initialize SDL: %s\n", SDL_GetError());
+        SDL_Log("SDL_Init(0) failed: %s\n", SDL_GetError());
         return 1;
     }
-    if (IMG_Init(0) == 0) {
-        SDL_Log("No image formats supported\n");
-    }
+
+    FOREACH_INIT_FLAGS(TEST_INIT_FLAG)
+
     IMG_Quit();
     SDL_Quit();
     return 0;
