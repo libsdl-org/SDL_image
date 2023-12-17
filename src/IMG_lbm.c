a38dd1180ceb23fc076de89fce3b4c924d140fbd
Ankith <itsankith26@gmail.com>
2023-12-17 20:06:07 +0530

    Fix regression (segfault) in LBM image loading

--- src/IMG_lbm.c
+++ src/IMG_lbm.c
@@ -259,11 +259,9 @@ SDL_Surface *IMG_LoadLBM_RW( SDL_RWops *src )
           format = SDL_PIXELFORMAT_BGR24;
 #endif
        }
-       if (nbplanes == 24 || flagHAM == 1) {
-          if ((Image = SDL_CreateRGBSurfaceWithFormat(0, width, bmhd.h, 0, format)) == NULL ){
-             goto done;
-          }
-       }
+        if ((Image = SDL_CreateRGBSurfaceWithFormat(0, width, bmhd.h, 0, format)) == NULL){
+            goto done;
+        }
     }
 
     if ( bmhd.mask & 2 )               /* There is a transparent color */
