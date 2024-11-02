0edd4af45c70afb6dbf82c66d76f5bbb2178f8c2
Anonymous Maarten <anonymous.maarten@gmail.com>
2024-11-02 04:28:03 +0100

    create-release.py: use head of git ref as default commit
    [ci skip]

--- build-scripts/create-release.py
+++ build-scripts/create-release.py
@@ -10,30 +10,33 @@ import subprocess
 ROOT = Path(__file__).resolve().parents[1]
 
 
-def determine_project() -> str:
+def determine_remote() -> str:
     text = (ROOT / "build-scripts/release-info.json").read_text()
     release_info = json.loads(text)
+    if "remote" in release_info:
+        return release_info["remote"]
     project_with_version = release_info["name"]
     project, _ = re.subn("([^a-zA-Z_])", "", project_with_version)
-    return project
+    return f"libsdl-org/{project}"
 
 
 def main():
-    project = determine_project()
-    default_remote = f"libsdl-org/{project}"
-
-    current_commit = subprocess.check_output(["git", "rev-parse", "HEAD"], cwd=ROOT, text=True).strip()
+    default_remote = determine_remote()
 
     parser = argparse.ArgumentParser(allow_abbrev=False)
     parser.add_argument("--ref", required=True, help=f"Name of branch or tag containing release.yml")
     parser.add_argument("--remote", "-R", default=default_remote, help=f"Remote repo (default={default_remote})")
-    parser.add_argument("--commit", default=current_commit, help=f"Commit (default={current_commit})")
+    parser.add_argument("--commit", help=f"Input 'commit' of release.yml (default is the hash of the ref)")
     args = parser.parse_args()
+
+    if args.commit is None:
+        args.commit = subprocess.check_output(["git", "rev-parse", args.ref], cwd=ROOT, text=True).strip()
 
 
     print(f"Running release.yml workflow:")
-    print(f"  commit = {args.commit}")
     print(f"  remote = {args.remote}")
+    print(f"     ref = {args.ref}")
+    print(f"  commit = {args.commit}")
 
     subprocess.check_call(["gh", "-R", args.remote, "workflow", "run", "release.yml", "--ref", args.ref, "-f", f"commit={args.commit}"], cwd=ROOT)
 
