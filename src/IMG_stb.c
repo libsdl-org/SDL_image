23869c1f6453393751e442f96d98969e3b70447f
Sam Lantinga <slouken@libsdl.org>
2025-02-10 11:08:26 -0800

    Removed unnecessary STB directives
    STBI_NO_GIF and STBI_NO_HDR are automatically enabled by specifying STBI_ONLY_PNG and STBI_ONLY_JPEG

--- src/IMG_stb.c
+++ src/IMG_stb.c
@@ -49,8 +49,6 @@
 #define STBI_NO_STDIO
 #define STBI_ONLY_PNG
 #define STBI_ONLY_JPEG
-#define STBI_NO_GIF
-#define STBI_NO_HDR
 #define STBI_NO_LINEAR
 #define STBI_ASSERT SDL_assert
 #define STB_IMAGE_IMPLEMENTATION
