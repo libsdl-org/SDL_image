a27e2edc95a16ae4c6558c5f244da36b93cb4f92
InigoMontoya <ohmysac@gmail.com>
2025-02-14 10:29:37 -0500

    formatting #3

--- src/IMG_webp.c
+++ src/IMG_webp.c
@@ -414,11 +414,11 @@ IMG_Animation *IMG_LoadWEBPAnimation_RW(SDL_RWops *src)
 
     SDL_FreeSurface(prevCanvas);
     
-	SDL_FreeSurface(canvas);
+    SDL_FreeSurface(canvas);
     
-	lib.WebPDemuxDelete(demuxer);
+    lib.WebPDemuxDelete(demuxer);
     
-	SDL_free(raw_data);
+    SDL_free(raw_data);
 
     return anim;
 
