f1e4fff0c92026cec5e6a7a3e3086047fba20cf1
Sam Lantinga <slouken@libsdl.org>
2025-03-03 13:05:29 -0800

    Set the alpha channel for less than 32-bit ICO/CUR images
    Fixes https://github.com/libsdl-org/SDL_image/issues/532
    
    (cherry picked from commit d4153324124963b8003e4e5b1d6743661969fdf5)

--- src/IMG_bmp.c
+++ src/IMG_bmp.c
@@ -307,6 +307,11 @@ LoadICOCUR_RW(SDL_RWops * src, int type, int freesrc)
         }
         for (i = 0; i < (int) biClrUsed; ++i) {
             SDL_RWread(src, &palette[i], 4, 1);
+
+            /* Since biSize == 40, we know alpha is reserved and should be zero, meaning opaque */
+            if ((palette[i] & 0xFF000000) == 0) {
+                palette[i] |= 0xFF000000;
+            }
         }
     }
 
@@ -361,7 +366,7 @@ LoadICOCUR_RW(SDL_RWops * src, int type, int freesrc)
                 Uint32 pixel;
                 Uint8 channel;
                 for (i = 0; i < surface->w; ++i) {
-                    pixel = 0;
+                    pixel = 0xFF000000;
                     for (j = 0; j < 3; ++j) {
                         /* Load each color channel into pixel */
                         if (!SDL_RWread(src, &channel, 1, 1)) {
