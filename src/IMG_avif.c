5c26a1d5cba626c8f62e8c81bb6e9e60b52e5de7
Xen <lordofxen@deskasoft.com>
2025-08-06 04:54:20 +0300

    Fix spacing
    Co-authored-by: Sam Lantinga <slouken@libsdl.org>

--- src/IMG_avif.c
+++ src/IMG_avif.c
@@ -1425,8 +1425,7 @@ bool IMG_CreateAVIFAnimationStream(IMG_AnimationStream *stream, SDL_PropertiesID
 
     int availableLCores = SDL_GetNumLogicalCPUCores();
     int mThreads = (int)SDL_GetNumberProperty(props, "maxthreads", availableLCores / 2);
-    if (mThreads < 1 || mThreads > availableLCores)
-    {
+    if (mThreads < 1 || mThreads > availableLCores) {
         return SDL_SetError("Invalid maxthreads value: %i must be in between 1 and %i", mThreads, availableLCores);
     }
 
