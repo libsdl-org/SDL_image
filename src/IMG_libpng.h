8c909940dad460a019f90cf8d24169b646d338be
Xen <lordofxen@deskasoft.com>
2025-08-09 08:44:36 -0700

    Update PNG Header - Removal of Stream Suffix

--- src/IMG_libpng.h
+++ src/IMG_libpng.h
@@ -19,5 +19,5 @@
   3. This notice may not be removed or altered from any source distribution.
 */
 
-extern bool IMG_CreateAPNGAnimationEncoderStream(IMG_AnimationEncoderStream *stream, SDL_PropertiesID props);
-extern bool IMG_CreateAPNGAnimationDecoderStream(IMG_AnimationDecoderStream *stream, SDL_PropertiesID decoderProps);
+extern bool IMG_CreateAPNGAnimationEncoder(IMG_AnimationEncoder *encoder, SDL_PropertiesID props);
+extern bool IMG_CreateAPNGAnimationDecoder(IMG_AnimationDecoder *decoder, SDL_PropertiesID props);
