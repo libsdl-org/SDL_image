955dab8b93507b2b1501a1852c65256b5f20eb66
Xen <lordofxen@deskasoft.com>
2025-08-09 08:43:56 -0700

    Update GIF Header - Removal of Stream Suffix

--- src/IMG_gif.h
+++ src/IMG_gif.h
@@ -19,5 +19,5 @@
   3. This notice may not be removed or altered from any source distribution.
 */
 
-extern bool IMG_CreateGIFAnimationEncoderStream(IMG_AnimationEncoderStream *stream, SDL_PropertiesID props);
-extern bool IMG_CreateGIFAnimationDecoderStream(IMG_AnimationDecoderStream *stream, SDL_PropertiesID decoderProps);
+extern bool IMG_CreateGIFAnimationEncoder(IMG_AnimationEncoder *encoder, SDL_PropertiesID props);
+extern bool IMG_CreateGIFAnimationDecoder(IMG_AnimationDecoder *decoder, SDL_PropertiesID props);
