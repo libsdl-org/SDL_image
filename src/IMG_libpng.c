809b768d0f4389858df7b185d1f170cf17cc1e05
Xen <lordofxen@deskasoft.com>
2025-08-13 00:23:30 -0700

    Replace SDL_MAX_SINT32 with SDL_SIZE_MAX

--- src/IMG_libpng.c
+++ src/IMG_libpng.c
@@ -922,8 +922,8 @@ static SDL_Surface *decompress_png_frame_data(DecompressionContext* context, png
     if (data_size < 0) {
         goto error;
     }
-    if ((Uint64)data_size >= SDL_MAX_SINT32) {
-        SDL_SetError("data size >= sizeof(size_t)");
+    if ((Uint64)data_size >= SDL_SIZE_MAX) {
+        SDL_SetError("data size >= SDL_SIZE_MAX");
         goto error;
     }
     void *buffer = NULL;
