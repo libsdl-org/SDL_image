af21c2f11284e3d18dd4e66430be5a43f9612131
Xen <lordofxen@deskasoft.com>
2025-08-17 04:48:11 -0700

    Drop MAX_XML_CONTENT_LENGTH to 32 megabytes

--- src/xmlman.c
+++ src/xmlman.c
@@ -1,6 +1,6 @@
 #include <SDL3/SDL.h>
 
-#define MAX_XML_CONTENT_LENGTH 128 * 1024 * 1024 // 128 MB
+#define MAX_XML_CONTENT_LENGTH 32 * 1024 * 1024 // 32 MB
 
 static char* xml_escape(const char* str) {
     size_t new_len = 0;
