62ff90a973031f7a1d0730e786265196ae0d3cb6
Matthew Zavislak <elevenfive@users.noreply.github.com>
2025-11-03 09:23:19 -0800

    Cleanup
    - Fix cmake files to use INTERFACE_INCLUDE_DIRECTORIES
    - Remove duplicate ELF alignment check

--- Xcode/pkg-support/resources/share/cmake/SDL3_image/SDL3_imageConfig.cmake
+++ Xcode/pkg-support/resources/share/cmake/SDL3_image/SDL3_imageConfig.cmake
@@ -138,6 +138,7 @@ if(NOT TARGET SDL3_image::SDL3_image-shared)
             INTERFACE_SDL3_SHARED "ON"
             COMPATIBLE_INTERFACE_STRING "SDL_VERSION"
             INTERFACE_SDL_VERSION "SDL3"
+            INTERFACE_INCLUDE_DIRECTORIES "${_sdl3_image_framework_path}/Headers"
     )
 endif()
 set(SDL3_image_SDL3_image-shared_FOUND TRUE)
