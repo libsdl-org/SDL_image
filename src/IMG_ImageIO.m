a920a1261699737a8fa73eff6715e1afd1426be7
Sam Lantinga <slouken@libsdl.org>
2026-01-16 13:16:47 -0800

    Updated license of ImageIO loading code to Zlib
    This change is made with the written permission of Eric Wing

--- src/IMG_ImageIO.m
+++ src/IMG_ImageIO.m
@@ -1,11 +1,23 @@
 /*
- *  IMG_ImageIO.c
- *  SDL_image
- *
- *  Created by Eric Wing on 1/1/09.
- *  Copyright 2009 __MyCompanyName__. All rights reserved.
- *
- */
+  IMG_ImageIO.m: Support for loading images using the ImageIO library
+  Copyright (C) 2009-2026 Eric Wing <ewing.dev@playcontrol.net>
+
+  This software is provided 'as-is', without any express or implied
+  warranty.  In no event will the authors be held liable for any damages
+  arising from the use of this software.
+
+  Permission is granted to anyone to use this software for any purpose,
+  including commercial applications, and to alter it and redistribute it
+  freely, subject to the following restrictions:
+
+  1. The origin of this software must not be misrepresented; you must not
+     claim that you wrote the original software. If you use this software
+     in a product, an acknowledgment in the product documentation would be
+     appreciated but is not required.
+  2. Altered source versions must be plainly marked as such, and must not be
+     misrepresented as being the original software.
+  3. This notice may not be removed or altered from any source distribution.
+*/
 
 #if defined(__APPLE__) && !defined(SDL_IMAGE_USE_COMMON_BACKEND)
 
