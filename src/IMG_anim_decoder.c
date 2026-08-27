19e8ac5867fd9d2ebccd916413a03aaae214bb7d
Sugargenix LLC <41839667+MrColour@users.noreply.github.com>
2026-08-27 14:09:14 -0700

    Allow .avif extension

--- src/IMG_anim_decoder.c
+++ src/IMG_anim_decoder.c
@@ -212,7 +212,7 @@ IMG_AnimationDecoder *IMG_CreateAnimationDecoderWithProperties(SDL_PropertiesID 
         result = IMG_CreateANIAnimationDecoder(decoder, props);
     } else if (SDL_strcasecmp(type, "apng") == 0 || SDL_strcasecmp(type, "png") == 0) {
         result = IMG_CreateAPNGAnimationDecoder(decoder, props);
-    } else if (SDL_strcasecmp(type, "avifs") == 0) {
+    } else if (SDL_strcasecmp(type, "avifs") == 0 || SDL_strcasecmp(type, "avif") == 0) {
         result = IMG_CreateAVIFAnimationDecoder(decoder, props);
     } else if (SDL_strcasecmp(type, "gif") == 0) {
         result = IMG_CreateGIFAnimationDecoder(decoder, props);
